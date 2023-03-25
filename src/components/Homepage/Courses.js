e711d04093cf4c03a575447faf1abaadb48e3354
289volts <289volts@gmail.com>
2023-03-25 18:52:58 +0100

    [FIX] Removed excess padding in benefits section

--- src/components/Homepage/Courses.js
+++ src/components/Homepage/Courses.js
@@ -29,7 +29,7 @@ const Courses = ({ clashDisplay }) => {
 	}, []);
 
 	return (
-		<section className="py-[2.5rem] courses relative md:py-[5rem]">
+		<section className="py-[2.5rem] courses relative md:pt-[5rem]">
 			<Image
 				src={blueStar}
 				alt="blue star"
